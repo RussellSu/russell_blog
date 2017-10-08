f7e70957eeca2c74d1b6d529f4c201e1623b06ea
xiao.su <xiao.su@innocellence.com>
2017-10-08 23:49:01 +0800

    marked：markdown编辑器

--- src/components/EditArticle.vue
+++ src/components/EditArticle.vue
@@ -9,17 +9,26 @@
       <h3 class="ar-title">作者</h3>
       <p v-text="article.author"></p>
     </div>
-    <div>
+    <div style="" class="p-l">
       <h3 class="ar-title">正文</h3>
       <textarea v-model='article.text' name="" class="ar-text" cols="30" rows="10" placeholder="正文"></textarea>
+    </div>
+    <div class="p-r">
+      <h3>预览</h3>
+      <div v-html="compileMD" class="code-preview"></div>
     </div>
     <button @click='createNewArticle' class='create-btn'>创建</button>
   </section>
 </template>
 
 <script>
+import Marked from 'marked'
+import highlightjs from 'highlight.js'
+import 'highlight.js/styles/googlecode.css'
+
 export default {
   name: 'editArticle',
+  components: { Marked, highlightjs },
   data () {
     return {
       mainTitle: '编辑文章',
@@ -30,18 +39,41 @@ export default {
       }
     }
   },
+  computed: {
+    compileMD: function () {
+      return Marked(this.article.text, { sanitize: true })
+    }
+  },
   created () {
     document.title = '编辑'
   },
+  mounted () {
+    this.markdown()
+  },
   methods: {
     createNewArticle () {
-      this.$http.post('/api/articles', {data: JSON.stringify(this.article)})
+      this.$http.post('/api/articles', { data: JSON.stringify(this.article) })
         .then(res => {
           console.log(res.data)
         })
         .catch(err => {
           console.log(err.response)
         })
+    },
+    markdown () {
+      Marked.setOptions({
+        renderer: new Marked.Renderer(),
+        hightlight: function (code) {
+          return highlightjs.highlightAuto(code).value
+        },
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false
+      })
     }
   }
 }
@@ -51,5 +83,9 @@ export default {
 <style scoped>
 .ar-text {
   width: 80%;
+}
+
+.code-preview {
+  background-color: #000;
 }
 </style>
