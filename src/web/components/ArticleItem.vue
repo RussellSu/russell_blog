d80a95d9f51113fec248bed23e51f8d887dce268
xiao.su <suxiao945@outlook.com>
2018-08-21 16:21:34 +0800

    comment style

--- src/web/components/ArticleItem.vue
+++ src/web/components/ArticleItem.vue
@@ -6,43 +6,54 @@
       <div class="float-right">更新时间:{{article.updateTime | time_format('YYYY/MM/DD HH:mm:ss')}}</div>
     </div>
     <div class="mark-body" v-html="compileMD"></div>
-    <div class="comments-wrapper">
-      <h3>评论</h3>
-      <div v-if="comments.length">
-        <div class="comment" v-for="comment in comments">
-          <div class="comment-user">
-            <span v-text="comment.user.fullname"></span>
-            <span v-text="comment.user.email"></span>
+    <div class="comments-wrapper clearfix">
+      <p class="comment-header">评论</p>
+      <dl v-if="article.comments.length">
+        <dd class="comment clearfix" v-for="comment of article.comments" :key="comment._id">
+          <div class="comment-left pull-left text-center">
+            <div class="user-thumbnail" :style="{'background-image':`url(${comment.user.thumbnail})`}"></div>
+            <i class="like-btn" @click="likeComment">like</i>
           </div>
-          <div class="comment-body">
-            <span>{{comment.text}}</span>
+          <div class="comment-right pull-right">
+            <div class="comment-user">
+              <span v-text="comment.user.fullname"></span>
+               - 
+              <span>{{comment.createTime | time_format('YYYY/MM/DD HH:mm:ss')}}</span>
+            </div>
+            <div class="comment-body">
+              <span>{{comment.text}}</span>
+            </div>
+            <div class="comment-replies" v-if="comment.replies.length">
+              <p class="reply" v-for="reply of comment.replies" :key="reply.text">
+                <span>{{reply.user.fullname}}{{reply.user.email}}{{reply.createTime | time_format('YYYY/MM/DD HH:mm:ss')}}</span>
+                <div>{{reply.text}}</div>
+              </p>
+            </div>
           </div>
-          <div class="comment-replies">
-            <p class="reply" v-for="reply in comment.replies">
-              <span>{{reply.user.fullname}}{{reply.user.email}}{{reply.createTime | time_format('YYYY/MM/DD HH:mm:ss')}}</span>
-              <div>{{reply.text}}</div>
-            </p>
-          </div>
-        </div>
-      </div>
+        </dd>
+      </dl>
       <strong class="text-center" v-else>无</strong>
     </div>
     <div class="comment-editor" v-show="showCommentEditor">
-      添加评论
+      发布评论
       <textarea name="" id="" cols="30" rows="10" v-model.trim="newComment"></textarea>
-      <button class="submit-btn pull-right" @click="submitComment">提交</button>
+      <button class="submit-btn pull-right" @click="submitComment">发布评论</button>
     </div>
     <button class="write-commont-btn btn" @click="writeComment" v-show="!showCommentEditor">评论</button>
   </section>
 </template>
 <script>
+import {mapState, mapGetters} from 'vuex'
 import Marked from 'marked'
 import highlightjs from 'highlight.js'
 import 'highlight.js/styles/googlecode.css'
 
 export default {
   name: 'articleItem',
-  components: { Marked, highlightjs },
+  components: {
+    Marked,
+    highlightjs,
+  },
   data() {
     console.log('data')
     return {
@@ -62,7 +73,13 @@ export default {
   computed: {
     compileMD: function() {
       return Marked(this.article.text, { sanitize: true })
-    }
+    },
+    ...mapState({
+
+    }),
+    ...mapGetters([
+      'isLogin'
+    ])
   },
   created() {
     console.log('created')
@@ -75,11 +92,15 @@ export default {
   },
   methods: {
     getData() {
+      let _this = this
+      this.$store.commit('ACTIVE_LOADING')
       this.$api.getArticleDetail(this.$route.params.id).then(res => {
+        _this.$store.commit('INACTIVE_LOADING')
         if (res.data.article) {
-          this.article = res.data.article
+          _this.article = res.data.article
         }
       }, err => {
+        _this.$store.commit('INACTIVE_LOADING')
         console.log(err)
       })
     },
@@ -102,6 +123,7 @@ export default {
       this.showCommentEditor = true
     },
     submitComment() {
+      let _this = this
       if (!this.newComment.length) {
         alert('你不打算说点啥吗')
         return
@@ -109,18 +131,40 @@ export default {
       this.$api.submitComment({
         article: this.article._id,
         text: this.newComment
+      }).then(res => {
+        _this.newComment = ''
+        _this.showCommentEditor = false
+        _this.getData()
+      }, err => {
+        if (err.response && err.response.data && err.response.data.notLogin) {
+          alert('请先登陆')
+        }
+        console.log(err.response)
       })
+    },
+    likeComment() {
+      alert('to be continued')
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 .mark-body {
+  margin: 1rem 0;
   padding: 1rem;
   background-color: rgba(255, 255, 255, 0.1);
 }
 .comments-wrapper {
   margin: 10px 0;
+}
+.comment-header {
+  border-bottom: 1px dotted #898989;
+  padding-bottom: 1rem;
+  font-size: 1.6rem;
+}
+.comment {
+  margin: 0 0 .5rem 0;
+  border-bottom: 1px dotted #898989;
 }
 .comment-editor {
   textarea {
@@ -131,7 +175,38 @@ export default {
 
   // }
 }
-
+.comment-left {
+  width: 4rem;
+  .like-btn {
+    display: block;
+    margin-top: 1.2rem;
+  }
+}
+.comment-right {
+  width: calc(100% - 4rem);
+  padding: .5rem;
+  box-sizing: border-box;
+  .comment-user {
+    height: 3rem;
+    font-size: 1.6rem;
+  }
+  .comment-body {
+    padding: 1rem;
+    border-radius: 4px;
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+}
+.user-thumbnail {
+  display: inline-block;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  vertical-align: middle;
+  background-color: #c5c5c5;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 @media screen and (max-width: 960px) {
   .write-commont-btn {
     bottom: 70px !important;
