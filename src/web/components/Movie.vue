c5e0fe6a62f3b17fc47ef232582923600dfa4f1a
xiao.su <suxiao945@outlook.com>
2018-10-18 18:52:40 +0800

    前台移除上传movie

--- src/web/components/Movie.vue
+++ src/web/components/Movie.vue
@@ -6,33 +6,15 @@
       <div class="float-right">更新时间:{{movie.updateTime | time_format('YYYY/MM/DD HH:mm:ss')}}</div>
     </div>
     <div class="video-wrapper">
-      <video v-if="movie.source" controls>
+      <video class="video" v-if="movie.source" controls>
         <source :src=movie.source type="video/mp4">
       </video>
     </div>
-
-    <uploader
-      :options="options"
-      :autoStart="false"
-      :fileStatusText="fileStatusText"
-      @file-success="fileSuccess"
-      class="uploader-example"
-      ref="uploader">
-      <uploader-unsupport></uploader-unsupport>
-      <uploader-drop class="drop-wrapper text-center">
-        <p>拖动文件到此处</p>
-        <p>或者</p>
-        <uploader-btn class="select-file-btn btn btn-sm">选择文件</uploader-btn>
-        <!-- <uploader-btn :attrs="attrs">select images</uploader-btn> -->
-        <!-- <uploader-btn :directory="true">select folder</uploader-btn> -->
-      </uploader-drop>
-      <uploader-list></uploader-list>
-    </uploader>
   </section>
 </template>
 <script>
 import { mapState, mapGetters } from 'vuex'
-import tool from '../utils/tool.js'
+// import tool from '../utils/tool.js'
 export default {
   name: 'movie',
   components: {
@@ -40,34 +22,6 @@ export default {
   data () {
     console.log('data')
     return {
-      md5: '',
-      options: {
-        // https://github.com/simple-uploader/Uploader/tree/develop/samples/Node.js
-        target: '/api/uploads/movie',
-        singleFile: true,
-        fileParameterName: 'file', // default: 'file'
-        testChunks: true,
-        successStatuses: [200, 201],
-        permanentErrors: [500, 501],
-        categoryMap: {
-          // image: ['gif', 'jpg', 'jpeg', 'png', 'bmp', 'webp'],
-          video: ['mp4', 'm3u8', 'rmvb', 'avi', 'swf', '3gp', 'mkv', 'flv'],
-          // audio: ['mp3', 'wav', 'wma', 'ogg', 'aac', 'flac'],
-          // document: ['doc', 'txt', 'docx', 'pages', 'epub', 'pdf', 'numbers', 'csv', 'xls', 'xlsx', 'keynote', 'ppt', 'pptx']
-        },
-        processResponse (res, cb) { // post 的回调
-          console.log('processResponse', res)
-          cb(null, res)
-        },
-        preprocess: this.preprocess,
-      },
-      fileStatusText: {
-        success: '成功',
-        error: '错误',
-        uploading: '上传中',
-        paused: '暂停',
-        waiting: '等待中'
-      },
       movie: {
         _id: '',
         name: '',
@@ -106,39 +60,6 @@ export default {
         console.log(err)
       })
     },
-    fileSuccess (rootFile, file, message, chunk) {
-      let _this = this
-      // message: post res
-      console.log('fileSuccess', rootFile, file, message, chunk)
-      this.$api.mergeChunk({
-        'identifier': rootFile.uniqueIdentifier,
-        'chunks': rootFile.chunks.map(item => ({ chunkNum: (item.offset + 1).toString() })),
-        'filename': rootFile.name,
-        'mimeType': rootFile.fileType,
-      }).then(res => {
-        _this.$api.updateMovieAsset(_this.movie._id, { 'source': res.data.filePath }).then(res => {
-          _this.movie.source = res.data.source
-          alert('更新资源成功')
-        })
-      }, err => {
-        alert(err)
-      })
-    },
-    preprocess (chunk) {
-      let _this = this
-      console.log('preprocess chunk', chunk)
-      // 上传或test之前执行,生成md5(如果file存在md5 就不生成了)
-      if (!_this.md5) {
-        tool.getFileMD5(chunk.file.file, md5Value => {
-          _this.md5 = md5Value
-          chunk.file.uniqueIdentifier = md5Value
-          chunk.preprocessFinished()
-        })
-      }
-      else {
-        chunk.preprocessFinished()
-      }
-    },
   }
 }
 </script>
@@ -146,12 +67,7 @@ export default {
 .video-wrapper{
   text-align: center;
 }
-.drop-wrapper {
-  background-color: #DDDAD6;
-  width: 400px;
-  margin: 20px auto;
-}
-.select-file-btn {
-
+.video{
+  width: 100%;
 }
 </style>
